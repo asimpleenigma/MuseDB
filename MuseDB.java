100be842177f02012ace7043e0394d83fea10011
Benjamin Lloyd Cloer <asimpleenigma@gmail.com>
2017-11-12 14:47:10 -0500

    Update MuseDB.java
    We have a pause/play button!

--- MuseDB.java
+++ MuseDB.java
@@ -3,28 +3,20 @@
 package musedb;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
+import javafx.scene.*;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
-/*
-import java.io.File;
-import java.nio.file.Files;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
+//import java.beans.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+//import java.awt.event.ActionEvent;
 
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.shape.Circle;
-import javafx.stage.Stage;
-import java.nio.file.Paths;
-*/
+
 /**
  *
  * @author Lloyd Cloer
  */
+
 public class MuseDB extends Application {
     Player player;
     
@@ -32,10 +24,44 @@ public class MuseDB extends Application {
     public void start(Stage stage) {
         
         stage.setTitle("MuseDB");
+        
+        Group root = new Group();
+        
+        
+        
+        
+        //******* Test Player *******/
+        
+        Button play_button = new Button();
+        play_button.setText("Play");
+        play_button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                if (play_button.getText() == "Play"){
+                    player.play();
+                    play_button.setText("Pause");
+                }
+                else {
+                    player.pause();
+                    play_button.setText("Play");
+                }
+            }
+        });
+        
+        root.getChildren().add(play_button);
+        
+        // Test playing music
+        player = new Player();
+        player.selectSong();
+       // player.play();
+        
+        // Test moving files
+        String a1 = "C:\\Users\\Lloyd Cloer\\Documents\\Java Test\\Green Neuron.jpg";
+        String a2 = "C:\\Users\\Lloyd Cloer\\Documents\\Java Test\\the folder\\Green Neuron.jpg";
+      //  FileCoordinator.moveFile(a2, a1);
+      
+        stage.setScene(new Scene(root, 300, 250));
         stage.show();
         
-        player = new Player();
-        player.play();
     }
     
     public static void main(String[] args) {
