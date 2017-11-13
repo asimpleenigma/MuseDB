3b76c84b5fbd3001dae568ffe064caabc8c61773
Kyriell <The-K-Man@users.noreply.github.com>
2017-11-13 17:34:12 -0500

    updated FileCoord with metadata reading

--- FileCoordinator.java
+++ FileCoordinator.java
@@ -4,6 +4,18 @@
  * and open the template in the editor.
  */
 package musedb;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.Hashtable;
+import java.util.Set;
+
+import javax.imageio.ImageIO;
+
+import com.mpatric.mp3agic.ID3v2;
+import com.mpatric.mp3agic.InvalidDataException;
+import com.mpatric.mp3agic.Mp3File;
+import com.mpatric.mp3agic.UnsupportedTagException; 
+
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -34,4 +46,55 @@ public class FileCoordinator {
         }
         
     }
+
+    static Hashtable extractMetadata(String fileName){
+    
+        Hashtable<String, String> tempMetaData = new Hashtable<String, String>();
+        Mp3File mp3file;
+        //Everything happens in try/catch to negate any errors thrown from the file
+        try {
+          mp3file = new Mp3File(fileName);
+          if (mp3file.hasId3v2Tag()) {
+            ID3v2 id3v2Tag = mp3file.getId3v2Tag();
+            if (id3v2Tag.getTrack() != null){
+              tempMetaData.put("Track", (id3v2Tag.getTrack()));
+            }
+            if (id3v2Tag.getArtist() != null){
+              tempMetaData.put("Artist", (id3v2Tag.getArtist()));
+            }
+            if (id3v2Tag.getTitle() != null){
+              tempMetaData.put("Title", (id3v2Tag.getTitle()));
+            }
+            if (id3v2Tag.getAlbum() != null){
+              tempMetaData.put("Album", (id3v2Tag.getAlbum()));
+            }
+            if (id3v2Tag.getYear() != null){
+              tempMetaData.put("Year", (id3v2Tag.getYear()));
+            }
+            if ((Integer.toString(id3v2Tag.getGenre())) != null){
+              tempMetaData.put("Genre", id3v2Tag.getGenre()+" (" + id3v2Tag.getGenreDescription() + ")");
+            }
+            /**
+            byte[] imageData = id3v2Tag.getAlbumImage();
+            if (imageData != null && id3v2Tag.getTitle() != null) {
+                // Write image to file - can determine appropriate file extension from the mime type
+                // Depending on where you put this is where the file gets written to... we need to establish a folder?
+                RandomAccessFile file = new RandomAccessFile("C:\\Users\\kpari\\workspace\\museDB\\images\\"+(id3v2Tag.getTitle())+".jpg", "rw");
+                file.write(imageData);
+                file.close();
+            }
+            **/
+          }
+        }
+        catch (UnsupportedTagException e) {
+          e.printStackTrace();
+        }
+        catch (InvalidDataException e) {
+          e.printStackTrace();
+        }
+        catch (IOException e) {
+          e.printStackTrace();
+        }
+        return tempMetaData;
+      }
 }
