c2a4e3e1c0f0965969af0db4b3357eebc10b3c5b
Benjamin Lloyd Cloer <asimpleenigma@gmail.com>
2017-11-12 22:38:12 -0500

    Update FileCoordinator.java
    Supports imports (with makeshift temp database)

--- FileCoordinator.java
+++ FileCoordinator.java
@@ -7,11 +7,15 @@ package musedb;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.*;
+
+import java.io.File;
 /**
  *
  * @author Lloyd Cloer
  */
 public class FileCoordinator {
+    public static Hashtable<String, File> song_table = new Hashtable<String, File>();
     
     public static void moveFile(String source, String target){
         try{
@@ -21,5 +25,13 @@ public class FileCoordinator {
             ex.printStackTrace();
             System.out.println("Exception: " + ex.getMessage());
         }
+    }
+    
+    public static void importFiles(List<File> new_files){
+        for (File f : new_files){
+            song_table.put(f.getName(), f);
+            System.out.println(f.getName());
+        }
+        
     }
 }
