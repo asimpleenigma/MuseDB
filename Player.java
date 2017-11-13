e0bd401f990a2b3fe78adfce4285e2cfa3756f8e
Benjamin Lloyd Cloer <asimpleenigma@gmail.com>
2017-11-12 22:39:03 -0500

    Update Player.java
    Update to support user song selection

--- Player.java
+++ Player.java
@@ -1,4 +1,7 @@
 /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package musedb;
 
@@ -14,8 +17,8 @@ public class Player {
     MediaPlayer media_player;
     
     
-    public void selectSong(){
-        File file = new File("C:\\Users\\Lloyd Cloer\\Music\\Aesthetic Perfection\\Close To Human\\03 Architech.mp3");
+    public void selectSong(File file){
+        //File file = new File("C:\\Users\\Lloyd Cloer\\Music\\Aesthetic Perfection\\Close To Human\\03 Architech.mp3");
         try {
             Media media = new Media(file.toURI().toString());
             media_player = new MediaPlayer(media);
