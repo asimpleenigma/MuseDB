114ea886cb4d7e9cb4b43d276141193f9a2295f8
Benjamin Lloyd Cloer <asimpleenigma@gmail.com>
2017-11-21 17:45:38 -0500

    Scroll window, Menu Bar Import

--- MuseDB.java
+++ MuseDB.java
@@ -16,7 +16,12 @@ import javafx.event.EventHandler;
 import java.io.File;
 import javafx.stage.FileChooser;
 import java.util.*;
-
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 
 /**
  *
@@ -28,14 +33,36 @@ public class MuseDB extends Application {
     VBox song_listing;
     
     public static void main(String[] args) {
-        MuseDB muse = new MuseDB();
-        muse.launch();
+        String s = "C:\\Users\\Lloyd Cloer\\Music\\Aesthetic Perfection\\Close To Human\\03 Architech.mp3";
+        File file = new File(s);
+    //    Media media = new Media(s);
+        
+       // System.out.print
+     //   file = new File("file:/C:/Users/Lloyd%20Cloer/Music/AFI/AFI/01%20The%20Lost%20Souls.mp3");
+     //   Player p = new Player();
+       // p.selectSong(file);
+       // p.play();
+       // FileCoordinator.copyFile(file, "C:\\Users\\Lloyd Cloer\\Music\\");
+      //  System.out.println(FileCoordinator.extractMetadata(file));
+     //   Media media = new Media(file.toURI().toString());
+     //MediaPlayer media_player = new MediaPlayer(media);
+       //     System.out.println(media.getMetadata());
+        
+        if (true){
+            
+            MuseDB muse = new MuseDB();
+            muse.launch();
+        }
     }
     
     public void start(Stage stage) {
         
         // *** Initialize Model Classes *** //
         player = new Player();
+//        File file = new File("C:/Users/Lloyd%20Cloer/Music/AFI/AFI/01%20The%20Lost%20Souls.mp3");
+  //      player.selectSong(file);
+    //    player.play();
+        FileCoordinator fc = new FileCoordinator();
         
         
         stage.setTitle("MuseDB");
@@ -56,18 +83,29 @@ public class MuseDB extends Application {
         
         // **** File Import *** // 
         FileChooser file_chooser = new FileChooser();
-        Button import_button = new Button();
-        import_button.setText("Import");
-        import_button.setOnAction(
-            new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(final ActionEvent e) {
-                    List<File> list = file_chooser.showOpenMultipleDialog(stage);
-                    FileCoordinator.importFiles(list);
-                    listSongs(list);
-                }
-            });
-        control_panel.getChildren().add(import_button);
+        
+        
+        
+        // *** Menu Bar *** //
+        final Menu file_menu = new Menu("File");
+        MenuItem import_item = new MenuItem("Import");
+        import_item.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent t) {
+                List<File> list = file_chooser.showOpenMultipleDialog(stage);
+                fc.importFiles(list);
+                listSongs(list);
+            }
+        });
+        file_menu.getItems().add(import_item);
+        
+        final Menu menu2 = new Menu("Options");
+        final Menu menu3 = new Menu("Help");
+
+        MenuBar menu_bar = new MenuBar();
+        menu_bar.getMenus().addAll(file_menu, menu2, menu3);
+        
+        root.setTop(menu_bar);
+        //root
         
         //******* Test Player *******/
         
